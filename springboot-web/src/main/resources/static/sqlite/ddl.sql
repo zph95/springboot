4d848c0ad8d298257c5b83d0fdd5efe02fb06cd3
zph-programmer <1113955004@qq.com>
2021-02-12 20:37:16 +0800

    SQLite properties

--- springboot-web/src/main/resources/static/sqlite/ddl.sql
+++ springboot-web/src/main/resources/static/sqlite/ddl.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Rest_Call_Log_Record (
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	user_id INTEGER,
-	metod TEXT NOT NULL,
+	method TEXT NOT NULL,
 	uri TEXT NOT NULL,
 	request TEXT,
-	reponse TEXT,
+	response TEXT,
 	status INTEGER DEFAULT 0 NOT NULL,
 	cost_time INTEGER,
 	is_valid INTEGER DEFAULT 1 NOT NULL,
@@ -13,7 +13,6 @@ CREATE TABLE Rest_Call_Log_Record (
 );
 
 CREATE TRIGGER Rest_Call_Log_Record_Update  after update on Rest_Call_Log_Record
-FOR EACH ROW
 BEGIN
 	update Rest_Call_Log_Record set modified_time =datetime('now', 'localtime') where id=old.id;
 END
