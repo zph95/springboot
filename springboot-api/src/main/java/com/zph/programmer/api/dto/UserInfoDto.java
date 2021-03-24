d8fe9075633316010d173aa50e58a21d78a519a8
zph-programer <1113955004@qq.com>
2021-03-24 14:41:17 +0800

    update : pacakages

--- springboot-api/src/main/java/com/zph/programmer/api/dto/UserInfoDto.java
+++ springboot-api/src/main/java/com/zph/programmer/api/dto/UserInfoDto.java
@@ -1,9 +1,10 @@
 package com.zph.programmer.api.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 
 /**
  * @Author: zengpenghui
