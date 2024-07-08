b86b6fc7d18ca4973a04e9b8ba80e55b6e49fa6a
zph95 <penghui159@outlook.com>
2024-07-08 15:13:50 +0800

    update drools

--- src/main/java/com/zph/programmer/springdemo/task/BaseTask.java
+++ src/main/java/com/zph/programmer/springdemo/task/BaseTask.java
@@ -1,6 +1,12 @@
 package com.zph.programmer.springdemo.task;
 
 import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.PostConstruct;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,5 +19,16 @@ public class BaseTask {
     @Scheduled(cron = "${job.base.task.cron}")
     public void doTask() {
         log.info("schedule,corn表达式");
+    }
+
+    // todo 未完成
+    @PostConstruct
+    public void init(){
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+        KieSession kieSession = kieContainer.newKieSession("ksession-rules");
+        kieSession.fireAllRules();
+        kieSession.dispose();
+        System.out.println("规则执行完毕");
     }
 }
